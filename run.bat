1fabac0dd223f3c54dd56f89cba352bfaedcb91e
Shater <dr.elshater@gmail.com>
2026-07-31 10:36:01 +0300

    Modify run.bat to launch Shater YouTube downloader
    Updated the script to start the Shater YouTube downloader and removed uv installation checks.

--- run.bat
+++ run.bat
@@ -1,19 +1,9 @@
 @echo off
 echo ===================================================
-echo   Starting Studio Video Customization Suite
+echo   Starting shater youtube downlader and Studio Video Customization Suite
 echo ===================================================
 
-:: Check if uv is installed, if not, install it via standalone installer
-where uv >nul 2>nul
-if %errorlevel% neq 0 (
-    echo [INFO] uv manager not found. Installing uv...
-    powershell -ExecutionPolicy Bypass -Command "irm https://astral.sh | iex"
-    set "PATH=%USERPROFILE%\.local\bin;%PATH%"
-)
-
-echo [INFO] Synchronizing dependencies and running project...
-:: uv run will automatically grab Python 3.11 and launch app.py securely
-uv run --python 3.12 yotube_downloader.py
-
+echo [INFO] Download , enjoy Shater youtube downlader...
+uv run yotube_downloader.py
 pause
 
