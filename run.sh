ae4a284095188f1aef0f10bf3cff3b27fbd15410
Shater <dr.elshater@gmail.com>
2026-07-31 10:33:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 echo "==================================================="
-echo "  Starting Studio Video Customization Suite"
+echo "  Starting shater youtube downlader and Studio Video Customization Suite"
 echo "==================================================="
 
-# Check for uv installation, install if missing
-if ! command -v uv &> /dev/null; then
-    echo "[INFO] uv manager not found. Installing uv..."
-    curl -LsSf https://astral.sh | sh
-    source $HOME/.local/bin/env
-fi
 
-echo "[INFO] Synchronizing dependencies and running project..."
-# Execute the app inside an isolated environment pinned to Python 3.11
-uv run --python 3.12 yotube_downloader.py
+
+echo "[INFO] Download , enjoy Shater youtube downlader..."
+
+uv run yotube_downloader.py
+
 
