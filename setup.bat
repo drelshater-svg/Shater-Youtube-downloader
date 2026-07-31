d9ef1a73dd8ed9effe497d89ecdf86fc1f6d2d26
Shater <dr.elshater@gmail.com>
2026-07-31 13:53:23 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 #This script assumes you already have Git and FFmpeg installed on your Windows system path
 @echo off
-powershell -ExecutionPolicy ByPass -c "irm https://astral.sh | iex"
+powershell -ExecutionPolicy ByPass -c "winget install --id=astral-sh.uv -e"
 set "PATH=%USERPROFILE%\.local\bin;%PATH%"
 git clone https://github.com/drelshater-svg/Shater-Youtube-downloader.git
 cd Shater-Youtube-downloader
